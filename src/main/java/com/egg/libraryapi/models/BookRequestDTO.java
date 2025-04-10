7e8ed8142a1ac6b6c77497ecf333f34ca760981e
Guillermo F Funaro <funaro.g.f@gmail.com>
2025-04-10 06:57:41 -0300

    Pruebas con ModelMapper

--- src/main/java/com/egg/libraryapi/models/BookRequestDTO.java
+++ src/main/java/com/egg/libraryapi/models/BookRequestDTO.java
@@ -13,7 +13,7 @@ public class BookRequestDTO {
     private Long isbn;
     private Integer specimens;
     @NotBlank(message = "El título no puede estar vacío")
-    @Size(min = 5, max = 10, message = "El tamaño del texto debe estar entre 5 y 10 caracteres.")
+    @Size(min = 5, max = 100, message = "El tamaño del texto debe estar entre 5 y 10 caracteres.")
     private String bookTitle;
     private UUID idEditorial;
     private UUID idAuthor;
